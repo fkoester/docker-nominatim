f0a2f7f3a392bb43d70c2f58c7aa681c971aec44
Fabian Köster <mail@fabian-koester.com>
2017-01-05 00:02:08 +0100

    Try some postgres tuning params

---
+++ postgis/tune-postgres.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+# Taken from http://wiki.openstreetmap.org/wiki/Nominatim/Installation
+cat >> ${PGDATA}/postgresql.conf <<EOF
+shared_buffers = 1GB
+maintenance_work_mem = 5GB
+work_mem = 25MB
+effective_cache_size = 12GB
+synchronous_commit = off
+checkpoint_timeout = 10min
+checkpoint_completion_target = 0.9
+fsync = off
+full_page_writes = off
+EOF
