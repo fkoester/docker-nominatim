76cff2ee6800dd9fb14a0bd107e31d333f9eae81
Fabian Köster <mail@fabian-koester.com>
2017-01-05 08:40:09 +0100

    Revert "Try newer postgres version"
    This reverts commit 8fe54be18a53cb96509637b0d179e2d2639169f3.

--- nominatim/local.php
+++ nominatim/local.php
@@ -3,6 +3,6 @@
  @define('CONST_Database_DSN', 'pgsql://nominatim@postgis:5432/nominatim'); // <driver>://<username>:<password>@<host>:<port>/<database>
  @define('CONST_Website_BaseURL', '/');
  // Software versions
- @define('CONST_Postgresql_Version', '9.6');
+ @define('CONST_Postgresql_Version', '9.4');
  @define('CONST_Postgis_Version', '2.3');
 ?>
