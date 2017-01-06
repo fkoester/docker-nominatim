883e84940b3a61de998a7abccc1d4a98b4611f97
Fabian Köster <mail@fabian-koester.com>
2017-01-06 09:13:35 +0100

    Add script for transferring docker volume

---
+++ ssh-copy-database.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+## Based on https://www.guidodiepen.nl/2016/05/transfer-docker-data-volume-to-another-host/
+## by Guido Diepen
+
+TARGET_HOST=${1}
+VOLUME="dockernominatim_nominatim-database"
+
+if [ $# -lt 1 ]
+then
+  echo 'Usage: ssh-copy-database.sh <target host>'
+  exit -1
+fi
+
+docker run --rm -v dockernominatim_nominatim-database:/from alpine ash -c "cd /from ; tar -cf - . " | ssh ${TARGET_HOST} 'docker run --rm -i -v dockernominatim_nominatim-database:/to alpine ash -c "cd /to ; tar -xvf - " '
